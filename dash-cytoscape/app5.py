1ac6cb14d356df623e65f284967ede19ffef5ce5
devg1120 <devg1120@gmail.com>
2025-02-24 17:27:18 +0900

    up

--- dash-cytoscape/app5.py
+++ dash-cytoscape/app5.py
@@ -321,31 +321,45 @@ layout =  dcc.Dropdown(
 controls = dbc.Card(
     [
         layout,
-        html.Div(
-            [
-                dbc.Label("X variable"),
-                dcc.Dropdown(
-                    id="x-variable"
-                ),
-            ]
-        ),
-        html.Div(
-            [
-                dbc.Label("Y variable"),
-                dcc.Dropdown(
-                    id="y-variable",
-                ),
-            ]
-        ),
-        html.Div(
-            [
-                dbc.Label("Cluster count"),
-                dbc.Input(id="cluster-count", type="number", value=3),
-            ]
-        ),
-     html.Pre(id='cytoscape-tapNodeData-json', style=styles['pre']),
-     html.Button("Print elements JSONified", id="button-cytoscape"),
-     html.Div(id="html-cytoscape"),
+
+        html.Div(children=[
+            html.Label('Dropdown'),
+            dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+
+            html.Br(),
+            html.Label('Multi-Select Dropdown'),
+            dcc.Dropdown(['New York City', 'Montréal', 'San Francisco'],
+                         ['Montréal', 'San Francisco'],
+                         multi=True),
+
+            html.Br(),
+            html.Label('Radio Items'),
+            dcc.RadioItems(['New York City', 'Montréal', 'San Francisco'], 'Montréal'),
+        ], style={'padding': 10, 'flex': 1}),
+
+        html.Div(children=[
+            html.Label('Checkboxes'),
+            dcc.Checklist(['New York City', 'Montréal', 'San Francisco'],
+                          ['Montréal', 'San Francisco']
+            ),
+
+            html.Br(),
+            html.Label('Text Input'),
+            dcc.Input(value='MTL', type='text'),
+
+            html.Br(),
+            html.Label('Slider'),
+            dcc.Slider(
+                min=0,
+                max=9,
+                marks={i: f'Label {i}' if i == 1 else str(i) for i in range(1, 6)},
+                value=5,
+            ),
+        ], style={'padding': 10, 'flex': 1}),
+
+        html.Pre(id='cytoscape-tapNodeData-json', style=styles['pre']),
+        html.Button("Print elements JSONified", id="button-cytoscape"),
+        html.Div(id="html-cytoscape"),
     ],
     body=True,
 )
